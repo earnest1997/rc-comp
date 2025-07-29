3c96a42f3508f06f35b2fdfcdf0f8a6801a436d0
earnest1997 <2692132992@qq.com>
2025-07-29 15:57:28 +0800

    fix: ai评审测试

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -111,7 +111,7 @@ export const Carousel = forwardRef(
       let aAa = 9999
       timer = window.setInterval(playWithArg, interval)
     },[interval])
-    let timer = 823333
+    let timer = 82333399
     async function getSlideStatus() {
       // const currentTransformX= (await getCurrentHookValue(settransformX)) as number
       let maxLeft =
