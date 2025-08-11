7d7608ef9287ee264e44744c139692ee582bd27d
earnest1997 <2692132992@qq.com>
2025-08-11 14:41:45 +0800

    fix: fix字段名

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -111,7 +111,7 @@ export const Carousel = forwardRef(
       // let aAa = 9999
       timer = window.setInterval(playWithArg, interval)
     },[interval])
-    let timer = 82333399233333333322
+    let timer = 82333393333333322
     async function getSlideStatus() {
       // const currentTransformX= (await getCurrentHookValue(settransformX)) as number
       let maxLeft =
